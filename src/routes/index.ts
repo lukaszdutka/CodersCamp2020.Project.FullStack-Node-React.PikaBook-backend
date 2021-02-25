16d5c49e981377ce422118eae9e18b5345af1fd0
AleksandraCyp <73715885+AleksandraCyp@users.noreply.github.com>
2021-02-25 19:43:35 +0100

    Add healthcheck endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,17 @@
-import { Router } from 'express';
-import UserRouter from './Users';
+import UserRouter from './Users';import StatusCodes from 'http-status-codes';
+import { Request, Response, Router } from 'express';
+const {  OK } = StatusCodes;
+
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
 router.use('/users', UserRouter);
+
+router.get('', (req: Request, res: Response) => {
+    return res.status(OK).send('Pikachu');
+});
 
 // Export the base-router
 export default router;
