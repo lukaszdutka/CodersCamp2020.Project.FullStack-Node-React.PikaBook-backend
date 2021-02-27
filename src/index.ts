b3a07ea4f6e8bcd7825f9372e137d155830f0779
Daria Dziubaltowska <daria305@gmail.com>
2021-02-27 09:13:28 +0100

    MNG-134 Add connection to db
    User/password to db are stored in development.env and gitignored
    Db created on Mongodb Atlas

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import './pre-start'; // Must be the first import
 import app from '@server';
 import logger from '@shared/Logger';
+import mongoose from 'mongoose'
 
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.svu8r.mongodb.net/Pikabook?retryWrites=true&w=majority`, {useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true}, () => {
+    console.log("connected to db");
+})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'Mongodb Connection Error'))
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
