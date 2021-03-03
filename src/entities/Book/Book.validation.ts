c9118e65e40acfcefbefd77e5f29124a7d38ebe5
Gosia Dziewit <gosia.dziewit@gmail.com>
2021-03-03 09:17:30 +0100

    book Joi validation

---
+++ src/entities/Book/Book.validation.ts
@@ -0,0 +1,24 @@
+import Joi, { ValidationResult } from 'joi';
+import BookReq from './Book.interface';
+
+const validateBookReq = (bookReq: BookReq): ValidationResult => {
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      author: Joi.array().items(Joi.string()),
+      genres: Joi.array().items(Joi.string()),
+      year: Joi.number(),
+      publisher: Joi.string(),
+      description: Joi.string(),
+      ownerId: Joi.string().pattern(/^[0-9a-fA-F]{24}$/)
+    })
+    return schema.validate(bookReq);
+}
+
+export default validateBookReq;
+
+// USE:
+//  router.post(...) => {
+//   const { error } = validateBookReq(req.body);
+//   if (error) return res.status(400).send(error.details[0].message);
+//   ...
+// }
