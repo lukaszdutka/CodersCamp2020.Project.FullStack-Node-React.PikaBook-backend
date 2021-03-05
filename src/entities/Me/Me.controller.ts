7694faa304a0014fb855b695f2fdb9e04698cbdf
AleksandraCyp <73715885+AleksandraCyp@users.noreply.github.com>
2021-03-05 10:45:09 +0100

    Change imports

--- src/entities/Me/Me.controller.ts
+++ src/entities/Me/Me.controller.ts
@@ -1,10 +1,8 @@
 import StatusCodes from 'http-status-codes';
 import { Request, Response } from 'express';
-import userModel from '../User/User.schema';
-import bookModel from '../../entities/Book/Book.schema';
+import User from '../User/User.schema';
+import Book from '../../entities/Book/Book.schema';
 
-const Book = bookModel;
-const User = userModel;
 const { BAD_REQUEST, OK } = StatusCodes;
 
 export const getLoggedUser = async (req: Request, res: Response) => {
