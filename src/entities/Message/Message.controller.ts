b0d151ab0fa3884224507754583dc54464153138
AleksandraCyp <73715885+AleksandraCyp@users.noreply.github.com>
2021-03-10 19:58:06 +0100

    Fix sending messages to yourself

--- src/entities/Message/Message.controller.ts
+++ src/entities/Message/Message.controller.ts
@@ -2,18 +2,21 @@ import StatusCodes from 'http-status-codes';
 import { Request, Response } from 'express';
 import validateMessageReq from './Message.validation';
 import Message from '../../entities/Message/Message.schema'
+import User from '../User/User.schema'
 
 const { BAD_REQUEST, CREATED } = StatusCodes;
 
 export const sendMessage = async (req: Request, res: Response) => {
     const { error } = validateMessageReq(req.body);
     if (error) return res.status(BAD_REQUEST).send(error.details[0].message);
-    if (req.body.recipient === req.user) {
+    const sender = await User.findById(req.user);
+    if (!sender) return res.status(BAD_REQUEST).send("The sender is not a logged user")
+    if (req.body.recipient == sender._id) {
         return res.status(BAD_REQUEST).send("You can't send a message to yourself")  
     } 
     const sentData = req.body;
     const defaultData = {
-        sender: req.user, 
+        sender: sender._id, 
         read: false
     }
     const message = new Message({...sentData, ...defaultData})
