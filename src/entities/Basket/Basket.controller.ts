25c80b762ceb4c31cc22d44285c4c363cef91469
mariusz-sm <mariusz.smarz@gmail.com>
2021-03-12 20:45:38 +0100

    Get basket by id controller

--- src/entities/Basket/Basket.controller.ts
+++ src/entities/Basket/Basket.controller.ts
@@ -5,6 +5,18 @@ import Basket from './Basket.schema'
 
 const { BAD_REQUEST, CREATED, OK } = StatusCodes;
 
+export const getBasketById = async (req: Request, res: Response) => {
+    try {
+        const basket = await Basket
+            .findById(req.params.id)
+            .populate('createdByUserId', 'name')
+            .populate('targetUserID', 'name')
+        return res.status(OK).json(basket);
+    } catch (error) {
+        return res.status(BAD_REQUEST).send(error.message);
+    }
+}
+
 export const addBasket = async (req: Request, res: Response) => {
     const { error } = validateBasketReq(req.body);
     if (error) return res.status(BAD_REQUEST).send(error.details[0].message);
--- src/routes/basket.ts
+++ src/routes/basket.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import checkToken from  'express-jwt';
-import { addBasket } from '@entities/Basket/Basket.controller';
+import { getBasketById, addBasket } from '@entities/Basket/Basket.controller';
 
 const router = Router();
 
 
 router
     // .get('', getBaskets)
-    // .get('/:id', getBasketById)
+    .get('/:id', getBasketById)
     .post('', checkToken({ secret: `${process.env.JWT_PRIVATE_KEY}`, algorithms: ['HS256']}), addBasket)
     // .put('/:id', updateBasket)
     // .delete('/:id', deleteBasket)
