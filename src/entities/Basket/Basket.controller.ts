7e1628749af4bebe48c4722f8fb5f5b27c68c572
mariusz-sm <mariusz.smarz@gmail.com>
2021-03-14 15:42:12 +0100

    Add validation for offered books

--- src/entities/Basket/Basket.controller.ts
+++ src/entities/Basket/Basket.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import validateBasketReq from './Basket.validation';
 import Basket from './Basket.schema';
 import User from '../User/User.schema'
+import Book from '../Book/Book.schema'
 
 const { BAD_REQUEST, CREATED, OK } = StatusCodes;
 
@@ -12,6 +13,16 @@ export const addBasket = async (req: Request, res: Response) => {
 
     const user = await User.findById(req.user)
     if (user?._id == req.body.targetUserID) return res.status(BAD_REQUEST).send('targetUserID should be different that createdByUserId')
+    
+    const booksToOffer = await Book.find({ownerId: req.user});
+    const booksToOfferId: string[] = booksToOffer.map( book => book._id.toString());
+    const booksToOfferFromRequest: string[] = req.body.booksOffered
+    if (!booksToOfferFromRequest.every(bookId => booksToOfferId.indexOf(bookId) > -1)) {
+        return res.status(BAD_REQUEST).send("You are offering a book that you don't have.")
+    }
+
+    // console.log(booksToOfferId)
+    // console.log(req.body.booksOffered)
 
     const basketData = req.body;
     const createdByUserId = {createdByUserId: req.user}
