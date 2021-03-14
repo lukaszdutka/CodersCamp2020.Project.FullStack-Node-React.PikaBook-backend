ede1c548ec5ce73e317610ecaa28b4f482c1cae5
Gosia Dziewit <gosia.dziewit@gmail.com>
2021-03-14 20:47:41 +0100

    added tests for user

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: [
-    "/node_modules/"
+    "/node_modules/", 
+    "./spec/test-setup.ts",
   ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testMatch: [
     "**/spec/**/*.ts?(x)",
     "**/?(*.)+(spec|test).ts?(x)"
