323f93d92c878f35310543dc518f4674641069ee
mariusz-sm <mariusz.smarz@gmail.com>
2021-03-14 14:35:20 +0100

    Check if targetUserID is different than createdByUserId

--- src/entities/Basket/Basket.controller.ts
+++ src/entities/Basket/Basket.controller.ts
@@ -1,7 +1,8 @@
 import StatusCodes from 'http-status-codes';
 import { Request, Response } from 'express';
 import validateBasketReq from './Basket.validation';
-import Basket from './Basket.schema'
+import Basket from './Basket.schema';
+import User from "../User/User.schema"
 
 const { BAD_REQUEST, CREATED, OK } = StatusCodes;
 
@@ -20,6 +21,10 @@ export const getBasketById = async (req: Request, res: Response) => {
 export const addBasket = async (req: Request, res: Response) => {
     const { error } = validateBasketReq(req.body);
     if (error) return res.status(BAD_REQUEST).send(error.details[0].message);
+    
+    const user = await User.findById(req.user)
+    if (user?._id == req.body.targetUserID) return res.status(BAD_REQUEST).send('targetUserID should be different that createdByUserId')
+    
     const basketData = req.body;
     const createdByUserId = {createdByUserId: req.user}
     const basket = new Basket({...basketData, ...createdByUserId})
