fca9fe158536390343c29ba8ba73c1067a049b88
lukaszdutka <lukaszdutka2@gmail.com>
2021-03-15 17:21:30 +0100

    try deploy on heroku

--- build.ts
+++ build.ts
@@ -18,7 +18,8 @@ logger.timestamp = false;
         // Remove current build
         await remove('./dist/');
         // Copy production env file
-        await copy('./src/pre-start/env/production.env', './dist/pre-start/env/production.env');
+        // await copy('./src/pre-start/env/production.env', './dist/pre-start/env/production.env');
+        logger.info('commented out copying.')
         // Copy back-end files
         await exec('tsc --build tsconfig.prod.json', './')
     } catch (err) {
