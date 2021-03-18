824440fc28f599e99b066b29fb116a2c80485835
Daria Dziubałtowska <44535712+daria305@users.noreply.github.com>
2021-03-18 23:05:43 +0100

    Heroku deploy

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,7 @@ if (!process.env.JWT_PRIVATE_KEY) {
     logger.err('FATAL ERROR: jwtPrivateKey is not defined')
     process.exit(1);
 }
-const dbUrl = `${process.env.DB_URL}`
-    .replace('<USER>', process.env.DB_USER as string)
-    .replace('<PASSWORD>', process.env.DB_PASSWORD as string);
+const dbUrl = `${process.env.DATABASE_URL}`;
 mongoose.connect(dbUrl, {useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true}, () => {
         logger.info("connected to db");
 })
