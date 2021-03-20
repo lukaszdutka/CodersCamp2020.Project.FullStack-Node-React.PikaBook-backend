978ea81bcf65f9b141ccdd99c4614c26c2414f64
Daria Dziubałtowska <44535712+daria305@users.noreply.github.com>
2021-03-20 12:23:06 +0100

    fix 3

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { authenticateUser } from '../entities/Auth/Auth.controller';
+import cors from "cors"
+
 
 const router = Router();
 
 // authenticate users
-router.post('/', authenticateUser);
+router.post('/', cors(), authenticateUser);
 
 export default router;
