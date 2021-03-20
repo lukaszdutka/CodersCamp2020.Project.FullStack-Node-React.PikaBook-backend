ee61e614f678ac499ded11576226dfb3db7bc3f3
Daria Dziubałtowska <44535712+daria305@users.noreply.github.com>
2021-03-20 12:54:59 +0100

    Why on earth

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,11 @@ var corsOptions = {
   }
 app.use(cors(corsOptions));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 if (!process.env.JWT_PRIVATE_KEY) {
     logger.err('FATAL ERROR: jwtPrivateKey is not defined')
     process.exit(1);
