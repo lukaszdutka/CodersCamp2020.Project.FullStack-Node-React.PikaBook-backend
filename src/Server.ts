8d549e31c58c615bf4a6783c86a6309352454745
Daria Dziubałtowska <44535712+daria305@users.noreply.github.com>
2021-03-20 13:05:47 +0100

    Please work

--- src/Server.ts
+++ src/Server.ts
@@ -34,8 +34,16 @@ if (process.env.NODE_ENV === 'development') {
 //     }));
 
 
+app.options('/api', function (req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.end();
+  })
+
 // Add APIs
 app.use('/api', BaseRouter);
+
 
 // Print API errors
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
