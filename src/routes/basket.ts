2fa362aaef9d0231760736a0a987b89313d82e10
AleksandraCyp <73715885+AleksandraCyp@users.noreply.github.com>
2021-04-15 12:30:46 +0200

    Update read

--- src/routes/basket.ts
+++ src/routes/basket.ts
@@ -9,6 +9,6 @@ router
     .get('/:id', checkToken({ secret: `${process.env.JWT_PRIVATE_KEY}`, algorithms: ['HS256']}), getBasketById)
     .post('', checkToken({ secret: `${process.env.JWT_PRIVATE_KEY}`, algorithms: ['HS256']}), addBasket)
     .put('/status/:id', checkToken({ secret: `${process.env.JWT_PRIVATE_KEY}`, algorithms: ['HS256']}), updateBasketStatus)
-    .put('/read', checkToken({ secret: `${process.env.JWT_PRIVATE_KEY}`, algorithms: ['HS256']}), updateBasketRead)
+    .put('/read/:id', checkToken({ secret: `${process.env.JWT_PRIVATE_KEY}`, algorithms: ['HS256']}), updateBasketRead)
 
 export default router;
